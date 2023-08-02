cb74384fe1bd1185575cc39068bb637c2adb7eb0
Yeliz Konduk <yelizkonduk@gmail.com>
2023-08-01 22:58:59 -0500

    assignment3

--- app/src/main/java/com/example/weatherapp/Api.kt
+++ app/src/main/java/com/example/weatherapp/Api.kt
@@ -6,14 +6,14 @@ import retrofit2.http.Query
 interface Api {
     @GET(value = "weather")
     suspend fun getWeatherData(
-        @Query("zip") zip: Int,
+        @Query("zip") zip: Int = 55318,
         @Query("units") units: String = "imperial",
         @Query("appid") appid: String = "a3754787a20fc3b59a7dca0a6f336bf4",
     ): WeatherData
 
     @GET(value = "forecast/daily")
     suspend fun getForecastData(
-        @Query("zip") zip: Int,
+        @Query("zip") zip: Int = 55318,
         @Query("units") units: String = "imperial",
         @Query("cnt") cnt: Int = 16,
         @Query("appid") appid: String = "a3754787a20fc3b59a7dca0a6f336bf4",
