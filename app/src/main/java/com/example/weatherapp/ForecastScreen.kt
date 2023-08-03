29583e1d3dcd44abe6f499d0137d1ca1efa6496b
Yeliz Konduk <yelizkonduk@gmail.com>
2023-08-03 00:58:28 -0500

    Assignmnet3

--- app/src/main/java/com/example/weatherapp/ForecastScreen.kt
+++ app/src/main/java/com/example/weatherapp/ForecastScreen.kt
@@ -20,14 +20,156 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.hilt.navigation.compose.hiltViewModel
+//added
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun EmptyView() {
+    Text(text = "No data available.")
+}
+
+@Composable
+fun TitleBar(title: String) {
+    Text(text = title, fontSize = 20.sp, modifier = Modifier.padding(16.dp))
+}
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun ForecastInfo(forecastItem: DayForecast) {
+    val formatTime: DateTimeFormatter = DateTimeFormatter.ofPattern("h:mm")
+    val formatMonth: DateTimeFormatter = DateTimeFormatter.ofPattern("MMM")
+    val sunriseTime =
+        LocalDateTime.ofInstant(
+            forecastItem.sunrise?.let { Instant.ofEpochSecond(it) },
+            ZoneId.of("America/Chicago")
+        )
+    val sunsetTime =
+        LocalDateTime.ofInstant(
+            forecastItem.sunset?.let { Instant.ofEpochSecond(it) },
+            ZoneId.of("America/Chicago")
+        )
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        AsyncImage(
+            model = forecastItem.iconUrl,
+            contentDescription = forecastItem.weather.firstOrNull()?.description,
+            modifier = Modifier.size(50.dp)
+        )
+        Text(
+            text = stringResource(
+                id = R.string.date,
+                sunriseTime.format(formatMonth),
+                sunriseTime.dayOfMonth
+            ),
+            fontSize = 14.sp
+        )
+        Spacer(modifier = Modifier.width(30.dp))
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(
+                text = stringResource(id = R.string.temperature, forecastItem.temp.day.toInt()),
+                fontSize = 12.sp
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Text(
+                text = stringResource(
+                    id = R.string.high_temperature,
+                    forecastItem.temp.max.toInt()
+                ),
+                fontSize = 12.sp
+            )
+        }
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(
+                text = "",
+                fontSize = 12.sp
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Text(
+                text = stringResource(
+                    id = R.string.low_temperature,
+                    forecastItem.temp.min.toInt()
+                ),
+                fontSize = 12.sp
+            )
+        }
+
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(
+                text = stringResource(id = R.string.sunrise, sunriseTime.format(formatTime)),
+                fontSize = 12.sp
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = stringResource(id = R.string.sunset, sunsetTime.format(formatTime)),
+                fontSize = 12.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun LoadingView() {
+    // show a loading
+    Text(text = "Loading...")
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ForecastScreen(viewModel: ForecastViewModel = hiltViewModel()) {
     val view = viewModel.weatherData.observeAsState()
+    val forecastList = view.value?.ForecastList  //added
     LaunchedEffect(Unit) {
         viewModel.viewAppeared()
 
     }
+    when {
+        forecastList == null && view.value == null -> LoadingView()
+        forecastList == null -> EmptyView()
+        else -> {
+            // Display the forecast
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TitleBar(stringResource(R.string.next_screen))
+                LazyColumn {
+                    itemsIndexed(forecastList) { index, item ->
+                        ForecastInfo(item)
+                    }
+                }
+            }
+        }
+    }
 }
+
+
 
 
 /*@Composable
